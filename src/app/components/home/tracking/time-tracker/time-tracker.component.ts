3fd432ee6a28f7e7458fcba0ac0ed9fe9fe3784e
Vincent <vjescasinas@gmail.com>
2023-04-12 19:58:17 +0800

    Add Time Tracker component & partial counter

--- src/app/components/home/tracking/time-tracker/time-tracker.component.ts
+++ src/app/components/home/tracking/time-tracker/time-tracker.component.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time-tracker.component.scss']
 })
 export class TimeTrackerComponent implements OnInit {
+  start: boolean = true;
+  stop: boolean = false;
+
+  startTime: any;
+  stopTime: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  startTimer() {
+    this.toggleStartStop();
+    this.startTime = new Date();
+    // const current_datetime = new Date();
+    // const amOrPm = current_datetime.getHours() >= 12 ? "PM" : "AM";
+    // console.log(current_datetime.getHours() + amOrPm)
+    // console.log(current_datetime.getMinutes())
+    // console.log(current_datetime.getSeconds())
+    var time = new Date();
+    console.log(
+      time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    );
+  }
+
+  stopTimer() {
+    this.toggleStartStop();
+    this.stopTime = new Date();
+    this.countTime();
+    var time = new Date();
+    console.log(
+      time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    );
+  }
+
+  toggleStartStop() {
+    this.start = !this.start;
+    this.stop = !this.stop;
+  }
+
+  countTime() {
+    console.log(this.stopTime.getTime() - this.startTime.getTime())
+    // console.log(new Date(this.stopTime - this.startTime).getTime())
   }
 
 }
