f448a6533404bb5b22c90f44ba4d4e5e0fcf2295
Vincent <vjescasinas@gmail.com>
2023-04-12 21:10:43 +0800

    Show minutes & seconds in frontend after starting timer

--- src/app/components/home/tracking/time-tracker/time-tracker.component.ts
+++ src/app/components/home/tracking/time-tracker/time-tracker.component.ts
@@ -11,6 +11,12 @@ export class TimeTrackerComponent implements OnInit {
 
   startTime: any;
   stopTime: any;
+  
+  hrs: string = "00";
+  mins: string = "00";
+  sec: string = "00";
+
+  intervalTimer: any;
 
   constructor() { }
 
@@ -20,6 +26,31 @@ export class TimeTrackerComponent implements OnInit {
   startTimer() {
     this.toggleStartStop();
     this.startTime = new Date();
+    
+    let hr = 0;
+    let min = 0;
+    let sec = 0;
+    this.intervalTimer = setInterval(() => {
+      sec++;
+      if(sec < 10) {
+        this.sec = "0" + sec.toString();
+      } else if(sec >= 60) {
+        sec = 0;
+        min++;
+        if(min < 10) {
+          this.mins = 0 + min.toString();
+        } else if(min >= 60) {
+          min = 0;
+          this.mins = 0 + min.toString();
+        } else {
+          this.mins = min.toString();
+        }
+        this.sec = "0" + sec.toString();
+      } else {
+        this.sec = sec.toString();
+      }
+      
+    }, 1000)
     // const current_datetime = new Date();
     // const amOrPm = current_datetime.getHours() >= 12 ? "PM" : "AM";
     // console.log(current_datetime.getHours() + amOrPm)
@@ -34,11 +65,13 @@ export class TimeTrackerComponent implements OnInit {
   stopTimer() {
     this.toggleStartStop();
     this.stopTime = new Date();
+    this.sec = "00";
     this.countTime();
     var time = new Date();
     console.log(
       time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
     );
+    clearInterval(this.intervalTimer);
   }
 
   toggleStartStop() {
